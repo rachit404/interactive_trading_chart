3716f9f28cdd5239050f1503de6e9d0f98fcbd4e
rachit404 <doshirachit28@gmail.com>
2025-06-01 15:55:52 +0530

    add tailwind

--- chart_ui/src/App.jsx
+++ chart_ui/src/App.jsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="bg-gray-600 text-white text-400 text-2xl font-bold p-4">
+        App
+      </div>
+    </>
+  );
 }
 
 export default App;
